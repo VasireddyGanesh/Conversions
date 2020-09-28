f6bee52327b91cd0c4458c2c3d8642145432163b
Prasad-Dp <dp.firebase.app@gmail.com>
2020-09-28 13:32:34 +0530

    Coversion Application Version 0.1

---
+++ Conversions-v0.1/app/src/main/java/com/appdevlop/conversions/MainActivity.java
@@ -0,0 +1,20 @@
+package com.appdevlop.conversions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Age(View view) {
+        startActivity(new Intent(MainActivity.this,AgeCaluclation.class));
+    }
+}
